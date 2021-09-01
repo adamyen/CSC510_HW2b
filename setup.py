41b33e605e55214bdcb0e9fce5cfb01bf4ddae6c
Arnold Lee <33062177+arnoldlee850807@users.noreply.github.com>
2021-08-31 23:43:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='CSC510_HW2B',
+      version='1.0',
+      description='CSC510 HW2B',
+      url='https://github.com/adamyen/CSC510_HW2b',
+      author='Adam, Arnold',
+      author_email='adamyen214@hotmail.com, arnold.kn.lee@gmail.com',
+      license='MIT',
+      packages=['CSC510_HW2B'],
+      install_requires=[],
+      zip_safe=False)
