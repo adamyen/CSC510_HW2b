759f5e3fd72d227df124d371d5113e8578b648b5
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-08-31 23:00:55 -0400

    Add power function to test.py

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
+import math
+
 # content of test_sample.py
 def inc(x):
     return x + 1
 
+def pow(base, exponent):
+    return math.pow(base, exponent)
 
 def test_answer():
     assert inc(3) == 4
+
+def test_pow():
+    assert pow(9, 3) = 729.0
