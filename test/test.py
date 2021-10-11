dc01fddb6e0a53096f95a2f5844ad5464f7496f1
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-10-11 16:00:13 -0400

    [core]: edit pytest script

--- test/test.py
+++ test/test.py
@@ -1,15 +1,10 @@
 # content of test_sample.py
-def inc(x):
-    return x + 1
 
 def pow(base, exponent):
     res = base
     for i in range(exponent - 1):
         res = res * base
     return res
-
-def test_answer():
-    assert inc(3) == 4
 
 def test_pow():
     assert pow(9, 3) == 729
