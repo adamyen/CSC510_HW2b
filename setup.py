b3a39b67a7793cf32b70f6b94b6e778b92c412ef
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-10-11 16:58:20 -0400

    [core]: edit setup

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-      name='CSC510_HW2B',
-      version='1.0',
-      description='CSC510 HW2B',
-      url='https://github.com/adamyen/CSC510_HW2b',
-      author='Adam, Arnold',
-      author_email='adamyen214@hotmail.com, arnold.kn.lee@gmail.com',
-      license='MIT',
-      packages=['code', 'test'],
+      name = "CSC510_HW2B",
+      version = "1.0",
+      description = "CSC510 HW2B",
+      author = "Adam, Arnold",
+      author_email = "adamyen214@hotmail.com, arnold.kn.lee@gmail.com",
+      license = "MIT",
+      packages = ["code", "test"],
 )
